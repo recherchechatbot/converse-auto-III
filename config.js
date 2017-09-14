677088487fbce5c19eb804acbfe32c8726176f1f
recherchechatbot <31929981+recherchechatbot@users.noreply.github.com>
2017-09-14 15:38:07 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+const config = {}
+
+config.apiaitoken = '812aca73113d4684b780de9821f22a7b'
+
+config.facebook = {
+    verification_token: 'converse_auto_III',
+    pageAccessToken: 'EAAIz3EmpCdEBANTX6reUZB7uZCVZBNLTYqcBJI9MRa2a2BvhjQ6ZAZARp3mKbVNUnmrU5BbZBN3JTPZAIzbl8aZBBCHot1XuIMh3vErDOtFq3MVZCQBaydUl3sxt6fqdpEchZC7KZAbnacEt2U3oxfg0uahFJCt490EA33DBFuPlkkLnwZDZD'
+}
+
+module.exports = config
+module.exports =
+    {
+        getConfig: function () {
+            return config;
+        }
+    };
